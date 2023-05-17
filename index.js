e675f907166ebcb058c4cf2b58cfc75f7d51b3f9
Christopher Lis <christopher4lis@gmail.com>
2023-05-16 21:16:17 -0400

    Move and rotate player

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-c.fillStyle = 'black'
-c.fillRect(0, 0, canvas.width, canvas.height)
-
 class Player {
   constructor({ position, velocity }) {
     this.position = position // {x, y}
     this.velocity = velocity
+    this.rotation = 0
   }
 
   draw() {
+    c.save()
+
+    c.translate(this.position.x, this.position.y)
+    c.rotate(this.rotation)
+    c.translate(-this.position.x, -this.position.y)
+
     c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false)
     c.fillStyle = 'red'
     c.fill()
 
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y, 100, 100)
+    c.beginPath()
     c.moveTo(this.position.x + 30, this.position.y)
     c.lineTo(this.position.x - 10, this.position.y - 10)
     c.lineTo(this.position.x - 10, this.position.y + 10)
@@ -27,6 +32,13 @@ class Player {
 
     c.strokeStyle = 'white'
     c.stroke()
+    c.restore()
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
   }
 }
 
@@ -35,6 +47,67 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 })
 
-player.draw()
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+}
 
-console.log(player)
+const SPEED = 3
+const ROTATIONAL_SPEED = 0.05
+const FRICTION = 0.97
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  player.update()
+
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED
+    player.velocity.y = Math.sin(player.rotation) * SPEED
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION
+    player.velocity.y *= FRICTION
+  }
+
+  if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED
+  else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true
+      break
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = false
+      break
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+  }
+})
